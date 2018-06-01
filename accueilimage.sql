35c6d6074db4004458849baf14007bb31862bd07
XuLeon1997 <38035723+XuLeon1997@users.noreply.github.com>
2018-06-01 16:21:30 +0200

    BDD des liens des images de la page d'accueil

---
+++ accueilimage.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 01 juin 2018 à 14:18
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `homemate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `accueilimage`
+--
+
+DROP TABLE IF EXISTS `accueilimage`;
+CREATE TABLE IF NOT EXISTS `accueilimage` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NomImage` varchar(20) NOT NULL,
+  `Chemin` varchar(250) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `accueilimage`
+--
+
+INSERT INTO `accueilimage` (`ID`, `NomImage`, `Chemin`) VALUES
+(1, 'Case1', 'images.jpg'),
+(2, 'Case2', 'maison.jpg'),
+(3, 'Case3', 'mais.jpg'),
+(4, 'Case4', 'tele.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
