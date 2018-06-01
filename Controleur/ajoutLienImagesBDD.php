1d515058cee869559ea31823bfdddd21ca997d6c
XuLeon1997 <38035723+XuLeon1997@users.noreply.github.com>
2018-06-01 16:13:24 +0200

    Contrôleur ajoutLienImagesBDD

---
+++ Controleur/ajoutLienImagesBDD.php
@@ -0,0 +1,6 @@
+
+<?php include('Modele/ajouterImageBDD.php');?>
+
+<?php echo "Les liens de la BDD ont bien été modifier"?>
+
+<?php echo "<a href= "index.php" target="_blank"> Retour à la page d'accuille ?</a>" ?>
