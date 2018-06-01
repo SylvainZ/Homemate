464ab52ce1ae31fc2bfaebef7e620c102fc0db3c
SylvainZ <38035677+SylvainZ@users.noreply.github.com>
2018-06-01 16:27:41 +0200

    messagerie

---
+++ Modele/boiteReceptionRecherche.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=homemate;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+/*
+$req = $bdd->query('SELECT * FROM messagerie ');
+*/
+	$numMessage=0;
+	
+// Récupération des 10 derniers messages
+$req = $bdd->query('SELECT * FROM messagerie WHERE Reception = \'Leon\' '); /*ORDER BY Date DESC LIMIT 0, 10*/
+// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+while ($donnees = $req->fetch())
+{
+	$sujet[] = $donnees['Sujet'];
+	$expediteur[] = $donnees['Expediteur'];
+	$date[] = $donnees['Date'];
+	$message[] = $donnees['Message'];
+	$reception[] = $donnees['Reception'];
+	$id[] = $donnees['ID'];
+	$corbeille[] = $donnees['Corbeille'];
+	$numMessage++;
+}
+$_SESSION['sujet']=$sujet;
+$_SESSION['expediteur']=$expediteur;
+$_SESSION['date']=$date;
+$_SESSION['message']=$message;
+$_SESSION['reception']=$reception;
+$_SESSION['id']=$id;
+$_SESSION['corbeille']=$corbeille;
+header('Location: index.php?cible=boiteMail');
+?>
