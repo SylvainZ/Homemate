22d4b9f5fe439ed50f05300bc548dae7b2f5761e
KevinY97 <38214811+KevinY97@users.noreply.github.com>
2018-06-04 15:02:22 +0200

    Add files via upload
    Pages admin

---
+++ Vue/administrateurCA.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administrateur - Capteur/Actionneur</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styleAdminCA.css">
+</head>
+
+<body>
+	<section id="blocs">
+		<div class="image" >
+			<img class="homemate" src="images/logo_nouvo.png">
+			<img class="profil" src="images/iconeProfil.png">
+		</div>
+
+		<div class="tete">
+			<ul>
+				<li class="decoration"><span class="padding">
+				<a href="administrateurUser.php">
+				Comptes Utilisateurs</a></span></p>
+				<li class="decoration" id="blue"><span class="padding"><a href="administrateurCA.php">Capteurs/Actionnaires</a></span></p>
+				<li class="decoration"><span class="padding"><a href="administrateurPersonnalisation.php">Personnalisation du site</a></span></p>
+			</ul>
+		</div>
+	</section>
+
+	<section class="bloc2">
+	
+
+	</section>
+
+
+
+</body>
+</html>
