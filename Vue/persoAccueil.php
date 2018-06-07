00ee1cf139d58c2871f381e078fd9a96f7f78602
KevinY97 <38214811+KevinY97@users.noreply.github.com>
2018-06-07 09:25:44 +0200

    Add files via upload

--- Vue/persoAccueil.php
+++ Vue/persoAccueil.php
@@ -3,17 +3,19 @@
 <head>
     <title>Personnalisation ACCUEIL</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/stylePersoAccueil.css">
+    <link rel="stylesheet" href="stylePersoAccueil.css">
+ 
 </head>
 
 <body>
-	<h1> Personnalisation de la page d'accueil</h1>
+	<p class="titre"> Personnalisation de la page d'accueil</p>
 	<section class="bigbloc">
 
 		<div class="bloc1">
 			<form method="post" action="../index.php?cible=ajoutLienImagesBDD">
 
 			<h1 style="color:white;"> Choisissez les images à affichées à la page d'accueil :</h1>
+			<br/>
 
 			<p> L'image de la case 1 : <select name="image1">
             	<option value="images.jpg" > image 1</option>
@@ -49,6 +51,20 @@
 		    <br/>
 		    <input type="submit" value="Valider">
 		    </form>
+
+		    <form method="post" action="reception.php" enctype="multipart/form-data" class="uploadImage">
+			    <h1 style="color:white;"> OU Upload une image personelle : </h1>
+			     <label for="case1">Image case 1 :</label><br />
+			     <input type="file" name="case1" id="case1"/><br />
+			     <label for="case2">Image case 2 :</label><br />
+			     <input type="file" name="case2"/><br/>
+			     <label for="case3">Image case 3 :</label><br />
+			     <input type="file" name="case3"/><br />
+			     <label for="case4">Image case 4 :</label><br />
+			     <input type="file" name="case4"/><br />
+			     <input type="submit" value="Envoyer" />
+			</form>
+		
 		</div>
 
 		<div class="bloc2">
