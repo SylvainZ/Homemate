d778592b278c5aad72a990b19563a9a3154a2bde
urchade <38214774+urchade@users.noreply.github.com>
2018-06-07 12:09:57 +0200

    Update profil.php

--- Vue/profil.php
+++ Vue/profil.php
@@ -19,48 +19,25 @@
     	<img src="Vue/images/iconeProfil.png" class="iconeProfil" alt="Icône Profil" /><span class="profil">Profil</span>
 
     	
-    	<div class="rectangle">
-    		<div class="col1">
-				<p>
-					Nom, Prénom, Statut: <br/>
-					<span class="valeurImporte"> <?php echo $_SESSION['nom'].', '.$_SESSION['prenom'];?></span><br/>
-					<span class="valeurImporte"> <?php echo $_SESSION['age'];?> ans </span><br/>
-					<span class="valeurImporte"> <?php echo $_SESSION['statut'];?></span><br/><br/>
-					Adresse complète:<br/>
-					<span class="valeurImporte"> Appartement <?php echo $_SESSION['numLogement'].', Etage '.$_SESSION['numEtage'];?></span><br/>
-					<span class="valeurImporte"> 
-					<?php if($_SESSION['numBis'] != 'NONE'){
-					    echo $_SESSION['numRue'].' '.$_SESSION['numBis'].' '.$_SESSION['nomRueBdAve'];?>  </span><br/>
-					<?php 
-					}
-					else
-					{
-					    echo $_SESSION['numRue'].' '.$_SESSION['nomRueBdAve'];?>  </span><br/>
-					    <?php 
-					}
-					?>
-					    
-					<span class="valeurImporte"> <?php echo $_SESSION['codePostal'].' '.$_SESSION['ville'];?> </span><br/><br/>
-					Mail:
-					<span class="valeurImporte"> <?php echo $_SESSION['email'];?></span><br/><br/>
-					Numéro de téléphone:
-					<span class="valeurImporte"> 0<?php echo $_SESSION['numTel'];?></span><br/>
-						
-					<a href="index.php?cible=modifieProfil	" ><input type=button value = "Modifier" class= "modifProfil"/></a> 
-	    	</div>
-	    	<div class="col2">
-				<p>
-					<a href="index.php?cible=boiteMailReception"><img src="Vue/images/IconeMail.png" class="image" alt="image du mail"/></a>
-					<a href="index.php?cible=boiteMailReception"><span>Boîte de réception</span></a><br/><br/>
-					<!--  <img src="Vue/images/iconeFacture.png" class="image" alt="image du facture" href="pageDAccueil.php"/>
-					<span href="Vue/pageDAccueil.php">Facture</span><br/><br/>
-					<img src="images/iconeAutorisation.png" class="image" alt="icône d'une famille" href="pageDAccueil.php"/>
-					<span href="Vue/pageDAccueil.php">Autorisation</span><br/><br/> -->
-					<a href="index.php?cible=capteurActionneursHabitations"><img src="Vue/images/iconeMaisonArbre.png" class="image" alt="icône du maison" /></a>
-					<a href="index.php?cible=capteurActionneursHabitations"><span>Gérer la maison</span></a><br/><br/>
-		    	</p>
-	    	</div>
-    	</div>
+    	        <div class="bar">
+         <div class="aff">
+        
+            <h1>Votre profil</h1>
+            <div class="br">
+            Nom: <?php echo $_SESSION['nom'];?><br>
+            Prenom: <?php echo $_SESSION['prenom'];?><br>
+            Age: <?php echo $_SESSION['age'];?> ans<br>
+            Statut: <?php echo $_SESSION['statut'];?><br>
+            Email: <?php echo $_SESSION['email'];?><br>
+            Telephone: <?php echo $_SESSION['numTel'];?><br>
+            Addresse: <?php echo $_SESSION['numRue'].' '.$_SESSION['nomRueBdAve'].' '.$_SESSION['numLogement'].' '.$_SESSION['codePostal'].' '.$_SESSION['ville'];?><br> 
+            </div>
+        </div>
+        <div >
+         	<a href="index.php?cible=modifieProfil	" ><input type=button value = "Modifier le profil"/></a> <br>   
+			
+			<a href="index.php?cible=capteurActionneursHabitations"><input type=button value = "Gerer la maison"/></a><br/><br/>           
+        </div>
     	
     	
     	    <footer>
