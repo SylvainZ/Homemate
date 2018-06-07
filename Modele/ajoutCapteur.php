1f92158ae75f355e81564583d0b57e4847a509a0
SylvainZ <38035677+SylvainZ@users.noreply.github.com>
2018-06-07 20:19:17 +0200

    ajout capteur

--- Modele/ajoutCapteur.php
+++ Modele/ajoutCapteur.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 if(!empty($_POST['numero_de_serie'])&&!empty($_POST['seuil']))
 {
   include('connexionBD.php');
@@ -9,18 +9,16 @@ if(!empty($_POST['numero_de_serie'])&&!empty($_POST['seuil']))
     $nom_du_capteur = $_POST['nom_du_capteur'];
     $type1 = explode('-',$_POST['nom_du_capteur']);
     $type = $type1[0];
-    $piece = $_POST['piece'];
     $description= $_POST['description'];
     
     if ($type=='Temperature')
     {
-        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,piece,seuilT,iduser,idpiece) VALUES(?,?,?,?,?,?,?,?)");
+        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,seuilT,iduser,idpiece) VALUES(?,?,?,?,?,?,?)");
         $req->execute(array(
             $type,
             $_POST['nom_du_capteur'],
             $_POST['numero_de_serie'],
             $_POST['description'],
-            $_POST['piece'],
             $_POST['seuil'],
             $_SESSION['ID'],
             $_GET['ID']
@@ -29,13 +27,12 @@ if(!empty($_POST['numero_de_serie'])&&!empty($_POST['seuil']))
     
     elseif ($type=='Luminosite')
     {
-        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,piece,seuilL,iduser,idpiece) VALUES(?,?,?,?,?,?,?,?)");
+        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,seuilL,iduser,idpiece) VALUES(?,?,?,?,?,?,?)");
         $req->execute(array(
             $type,
             $_POST['nom_du_capteur'],
             $_POST['numero_de_serie'],
             $_POST['description'],
-            $_POST['piece'],
             $_POST['seuil'],
             $_SESSION['ID'],
             $_GET['ID']
@@ -45,13 +42,13 @@ if(!empty($_POST['numero_de_serie'])&&!empty($_POST['seuil']))
     elseif  ($type=='Presence')
     {
         
-        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,piece,seuilD,iduser,idpiece) VALUES(?,?,?,?,?,?,?,?)");
+        $req = $bdd->prepare("INSERT INTO capteur(type,nom,NumSerie,Description,seuilD,iduser,idpiece) VALUES(?,?,?,?,?,?,?)");
         $req->execute(array(
             $type,
             $_POST['nom_du_capteur'],
             $_POST['numero_de_serie'],
             $_POST['description'],
-            $_POST['piece'],
+           
             $_POST['seuil'],
             $_SESSION['ID'],
             $_GET['ID']
