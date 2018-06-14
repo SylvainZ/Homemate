75e5e538846330676e17a8aff187dbe2be5b2072
urchade <38214774+urchade@users.noreply.github.com>
2018-06-14 16:25:25 +0200

    Update persoAccueil.php

--- Vue/persoAccueil.php
+++ Vue/persoAccueil.php
@@ -1,80 +1,69 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Personnalisation ACCUEIL</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="Vue/CSS/stylePersoAccueil.css">
- 
-</head>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Vue/CSS/acc.css"/>
+        <title>HOMEMATE</title>
+        <link rel="stylesheet" href="Vue/CSS/pageDacceuil.css">
+        <link rel="stylesheet" href="Vue/CSS/header.css">
+        <link rel="stylesheet" href="Vue/CSS/footer.css">
+        <link rel="stylesheet" href="Vue/CSS/all.css">
+        <meta charset="utf-8" />
 
-<body>
-	<p class="titre"> Personnalisation de la page d'accueil</p>
-	<section class="bigbloc">
 
-		<div class="bloc1">
-			<form method="post" action="index.php?cible=ajoutLienImagesBDD">
+    </head>
 
-			<h1 style="color:white;"> Choisissez les images à affichées à la page d'accueil :</h1>
-			<br/>
+    <body >
 
-			<p> L'image de la case 1 : <select name="image1">
-            	<option value="images.jpg" > image 1</option>
-            	<option value="maison.jpg">image 2</option>
-            	<option value="mais.jpg">image 3</option>
-            	<option value="tele.jpg">image 4</option>
-            	<option value="ori.jpg">image 5</option>
-		    </select>
+        <header>
+            <?php include("Vue/header.php") ?>
+        </header>
 
-		    <p> L'image de la case 2 : <select name="image2">
-            	<option value="images.jpg" > image 1</option>
-            	<option value="maison.jpg">image 2</option>
-            	<option value="mais.jpg">image 3</option>	 
-            	<option value="tele.jpg">image 4</option>
-            	<option value="ori.jpg">image 5</option>
-		    </select>
+            <table>
+                <tr>
+                    <td>
+                        <div class="titre">
+                            <h1>Notre entreprise</h1>
+                        </div>
+                            <br>
+                        <div class="titre2">
+                            Notre entreprise
+                        </div>
+                            <br>
+                        <div class="titre">
+                            <h1>Nos Capteurs</h1>
+                        </div>
+                            <br>
+                        <div class="titre2">
+                            Capteur 1
+                        </div>
+                    </td>
 
-		    <p> L'image de la case 3 : <select name="image3">
-            	<option value="images.jpg" > image 1</option>
-            	<option value="maison.jpg">image 2</option>
-            	<option value="mais.jpg">image 3</option>	 
-            	<option value="tele.jpg">image 4</option>
-            	<option value="ori.jpg">image 5</option>
-		    </select>
+                    <td>
+                        <div class="titre3">
+                            <span class="ecriture">Adresse :</span> <br><br>
+                            <span class="ecriture2">10 rue de Vanves </span> <br>
+                            <span class="ecriture2">Issy les Moulinaux</span> <br>
+                            <span class="ecriture2">92130</span> <br>
+                            <span class="ecriture2">France</span> <br><br>
+                            <span class="ecriture">Contacts :</span> <br><br>
+                            <span class="ecriture2">-Numéro de téléphone </span> <br>
+                            <span class="ecriture3"> 06 45 67 23 90 </span> <br>
+                            <span class="ecriture2">-Email </span> <br>
+                            <span class="ecriture3"> domisep@isep.fr </span> <br><br>
+                        </div>
+                            <br>
+                        <div class="titre3">
+                        </div>
+                    </td>
+                </tr>
+            </table>
 
-		    <p> L'image de la case 4 : <select name="image4">
-            	<option value="images.jpg" > image 1</option>
-            	<option value="maison.jpg">image 2</option>
-            	<option value="mais.jpg">image 3</option>	 
-            	<option value="tele.jpg">image 4</option>
-            	<option value="ori.jpg">image 5</option>
-		    </select>
-		    <br/>
-		    <input type="submit" value="Valider">
-		    </form>
 
-		    <form method="post" action="index.php?cible=controleImage" enctype="multipart/form-data" class="uploadImage">
-			    <h1 style="color:white;"> OU Upload une image personelle : </h1>
-			     <label for="case1">Image case 1 :</label><br />
-			     <input type="file" name="case1" id="case1"/><br />
-			     <label for="case2">Image case 2 :</label><br />
-			     <input type="file" name="case2"/><br/>
-			     <label for="case3">Image case 3 :</label><br />
-			     <input type="file" name="case3"/><br />
-			     <label for="case4">Image case 4 :</label><br />
-			     <input type="file" name="case4"/><br />
-			     <input type="submit" value="Envoyer" />
-			</form>
-		
-		</div>
 
-		<div class="bloc2">
-			<img src="Vue/images/images.jpg">
-			<img src="Vue/images/maison.jpg">
-			<img src="Vue/images/mais.jpg">
-			<img src="Vue/images/tele.jpg">
-			<img src="Vue/images/ori.jpg">
-		</div>
 
-	</section>
+        <footer>
+            <?php include("Vue/footer.php") ?>
+        </footer>
 
-</body>
+    </body>
+</html>
