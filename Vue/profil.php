3ed741e87ca77258168633548d7c6a521ccb74dc
XuLeon1997 <38035723+XuLeon1997@users.noreply.github.com>
2018-06-14 11:14:19 +0200

    Vue Profil Bouton ModifierMDP

--- Vue/profil.php
+++ Vue/profil.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 
 <html>
@@ -11,43 +10,42 @@
     </head>
 
     <body>
-
     	<header>
             <?php include("Vue/header.php") ?>
-
         </header>
-    	<br />
-        <div id="page">
-    	<img src="Vue/images/iconeProfil.png" class="iconeProfil" alt="Icône Profil" /><span class="profil">Profil</span>
-    	
-    	<br>
-    	<br>
-    	<br>
 
-    	
-        <div class="bar">
-         <div class="aff">
-        
-            
-            <div class="br">
-           <span class="texte"> Nom: <?php echo $_SESSION['nom'];?></span><br>
-           <span class="texte"> Prénom: <?php echo $_SESSION['prenom'];?></span><br>
-           <span class="texte"> Age: <?php echo $_SESSION['age'];?> ans</span><br>
-           <span class="texte"> Statut: <?php echo $_SESSION['statut'];?></span><br>
-           <span class="texte"> Email: <?php echo $_SESSION['email'];?></span><br>
-           <span class="texte"> Téléphone: 0<?php echo $_SESSION['numTel'];?></span><br>
-           <span class="texte"> Adresse: <?php echo $_SESSION['numRue'].' '; if ($_SESSION['numBis']!='NONE') {echo $_SESSION['numBis'].' ';} echo $_SESSION['prefixRue'].' '.$_SESSION['nomRueBdAve'].'<br/>';
-                   if ($_SESSION['typeHab']=='Appartement') {echo 'Appartement '.$_SESSION['numLogement'].' Etage '.$_SESSION['numEtage'].'<br/>';}
-               echo $_SESSION['codePostal'].' '.$_SESSION['ville'].'<br/>';
-               echo $_SESSION['pays']; ?></span><br>
+        <div id="page">
+            <img src="Vue/images/iconeProfil.png" class="iconeProfil" alt="Icône Profil" /><span class="profil">Profil</span>
+
+            <br>
+            <br>
+            <br>
+
+
+            <div class="bar">
+                 <div class="aff">
+
+
+                    <div class="br">
+                       <span class="texte"> Nom: <?php echo $_SESSION['nom'];?></span><br>
+                       <span class="texte"> Prénom: <?php echo $_SESSION['prenom'];?></span><br>
+                       <span class="texte"> Age: <?php echo $_SESSION['age'];?> ans</span><br>
+                       <span class="texte"> Statut: <?php echo $_SESSION['statut'];?></span><br>
+                       <span class="texte"> Email: <?php echo $_SESSION['email'];?></span><br>
+                       <span class="texte"> Téléphone: 0<?php echo $_SESSION['numTel'];?></span><br>
+                       <span class="texte"> Adresse: <?php echo $_SESSION['numRue'].' '; if ($_SESSION['numBis']!='NONE') {echo $_SESSION['numBis'].' ';} echo $_SESSION['prefixRue'].' '.$_SESSION['nomRueBdAve'].'<br/>';
+                               if ($_SESSION['typeHab']=='Appartement') {echo 'Appartement '.$_SESSION['numLogement'].' Etage '.$_SESSION['numEtage'].'<br/>';}
+                           echo $_SESSION['codePostal'].' '.$_SESSION['ville'].'<br/>';
+                           echo $_SESSION['pays']; ?></span><br>
+                    </div>
+                     <a href="index.php?cible=modifieProfil" ><input type=button class="boutonModifProfil" value = "Modifier le profil"/></a> <br>
+                </div>
+            <div >
+
+
+                <a href="index.php?cible=capteurActionneursHabitations"><input type=button value = "Gerer la maison"/></a><br/><br/>
+                <a href="index.php?cible=modifierMDP"><input type=button value = "Modifier votre mot de passe"/></a><br/><br/>
             </div>
-        </div>
-        <div >
-         	<a href="index.php?cible=modifieProfil" ><input type=button value = "Modifier le profil" class="boutonModif"/></a> <br>
-			
-			<a href="index.php?cible=capteurActionneursHabitations"><input type=button value = "Gerer la maison"/></a><br/><br/>           
-        </div>
-        </div>
         </div>
     	
 
