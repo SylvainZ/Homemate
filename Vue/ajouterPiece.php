b66fcc6181f5f82eca192edf5d4a7af597a73b34
Beatrice <beatrice.tesor@gmail.com>
2018-06-21 20:02:15 +0200

    style piece

--- Vue/ajouterPiece.php
+++ Vue/ajouterPiece.php
@@ -19,29 +19,38 @@
 		<form class="form1" method="post" action="index.php?cible=ajoutPiece2&ID=<?php echo $_GET['ID']?>">
 			<span class="piece2">Ajouter une pièce</span>
 
-			<div class="champnom ligne1">
-				<label for="nom" class="inputNom">Nom <br></label>
-				<input type="text" name="nom" id="nom"/><br>	
-			</div>
-
-			<div class="champnom ligne1 colonne1">
-				<label for="type" class="inputNom">Type</label><br>
-					<select name="type" id="type" required/>
-					<option value="salon">Salon</option>
-					<option value="salle de bain">Salle de bain</option>
-					<option value="cuisine">Cuisine</option>
-					<option value="chambre">Chambre</option>
-					<option value="garage">Garage</option>
-					<option value="toilettes">Toilettes</option>
-					<option value="autres">Autres</option>
-				</select>			
-				</div>
-
-			<div class="champnom ligne2">
-				<label for="Superficie" class="inputNom">Superficie</label><br>
-				<input type="text" name="superficie" id="superficie"/><br>
-			</div>
-					
+            <table>
+                <tr>
+                    <td>
+                        <div class="champnom ligne1">
+                            <label for="nom" class="inputNom">Nom <br></label>
+                            <input type="text" name="nom" id="nom"/>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="champnom ligne1 colonne1">
+                            <label for="type" class="inputNom">Type</label><br>
+                            <select name="type" id="type" required/>
+                                <option value="salon">Salon</option>
+                                <option value="salle de bain">Salle de bain</option>
+                                <option value="cuisine">Cuisine</option>
+                                <option value="chambre">Chambre</option>
+                                <option value="garage">Garage</option>
+                                <option value="toilettes">Toilettes</option>
+                                <option value="autres">Autres</option>
+                            </select>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="champnom ligne2">
+                            <label for="Superficie" class="inputNom">Superficie</label><br>
+                            <input type="text" name="superficie" id="superficie"/>
+                        </div>
+                    </td>
+                </tr>
+            </table>
 
 			<div class="valid">
 				<input type="submit" name="valider" value="Valider" class="bouton">
@@ -54,10 +63,7 @@
 	</div>
 </div>
 	    <footer>
-	    	<p>
-	Copyright 2018 HomeMate | Tous droits réservés
-			</p>
-		</footer>
+            <?php include("footer.php") ?>
     </body>
 
 </html>
