891e4f76b95df64265188f76a56149d114f32339
Leon <leon.xu@isep.fr>
2018-06-25 00:28:42 +0200

    Affichage courbe dans la fenetre modale

--- Controleur/recupDonneesPasserelle.php
+++ Controleur/recupDonneesPasserelle.php
@@ -77,16 +77,14 @@ function analyseTrame($bdd){
     $req_valeurCapteur = $bdd->query('SELECT valeurCapteur FROM passerelle ORDER BY ID DESC LIMIT 1');
     $valeurCapteur=$req_valeurCapteur->fetch();
     if (hexdec($valeurCapteur['valeurCapteur'])>900){
-        echo "Il y a quelqu'un";
     }
 
     else{
-        echo "Rien à signaler";
     }
     //echo hexdec($valeurCapteur['valeurCapteur']);
 }
 
-include('../Modele/donneesPasserelle.php');
+include('Modele/donneesPasserelle.php');
 
 ?>
 
--- Vue/capteur.php
+++ Vue/capteur.php
@@ -557,7 +557,7 @@ $nbColonne5=5;
     <div class="modal-dialog" role="document" >
         <div class="modal-content" >
             <div class="modal-header" style="background-color: #6a1b3d">
-                <span style="font-weight: bold; color: white">Modifier ses informations</span>
+                <span style="font-weight: bold; color: white">Courbe de distance depuis le capteur</span>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
